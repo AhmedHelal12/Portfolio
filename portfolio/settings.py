cd6774c6cccee596f1b1c93f3e846f850b87a1d1
AhmedHelal12 <ahmedhelalragab@gmail.com>
2025-02-01 18:47:27 +0200

    updating the mobile responsiveness

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ovod(cs8v8tk_8652*q^kpx3c)@px39of&aomb*73)u0#n+*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['Helal.pythonanywhere.com']
+ALLOWED_HOSTS =['Helal.pythonanywhere.com','127.0.0.1']
 
 # Application definition
 
