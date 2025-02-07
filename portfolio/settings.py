4eaab1b14402de49a61b404b47d56d6cfddde262
AhmedHelal12 <ahmedhelalragab@gmail.com>
2025-02-07 16:24:01 +0200

    Updating the navbar at mobile view

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -113,7 +113,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
@@ -127,7 +126,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
